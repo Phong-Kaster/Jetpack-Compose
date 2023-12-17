ab32d19e5bf71d72a7ddedadcea4dd7b489134ce
Phong <phongkaster@gmail.com>
2023-12-17 18:18:01 +0700

    optimize line chart

--- app/src/main/java/com/example/jetpack/ui/fragment/chart/component/LineChart.kt
+++ app/src/main/java/com/example/jetpack/ui/fragment/chart/component/LineChart.kt
@@ -3,7 +3,6 @@ package com.example.jetpack.ui.fragment.chart.component
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
