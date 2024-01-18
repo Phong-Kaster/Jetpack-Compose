ab855cb3d0bd84bb22aa66deac26c5a44c7f3f5a
phongapero <phongnt@apero.vn>
2024-01-18 18:39:47 +0700

    update content for README

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,7 +61,7 @@ android {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.compose.ui:ui")
@@ -132,7 +132,7 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     //implementation("com.squareup.okhttp3:okhttps:3.4.1")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 }
 
 // Allow references to generated code
