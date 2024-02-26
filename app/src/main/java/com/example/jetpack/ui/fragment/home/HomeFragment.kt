0f8f193368f2edaef46d2ef014b94c92b69e130c
phongapero <phongnt@apero.vn>
2024-02-26 13:43:41 +0700

    feature: add shimmer effect

--- app/src/main/java/com/example/jetpack/ui/fragment/home/HomeFragment.kt
+++ app/src/main/java/com/example/jetpack/ui/fragment/home/HomeFragment.kt
@@ -6,6 +6,7 @@ import android.os.Build
 import android.os.Bundle
 import android.view.View
 import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -14,8 +15,10 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -24,6 +27,7 @@ import com.example.jetpack.R
 import com.example.jetpack.core.CoreFragment
 import com.example.jetpack.core.CoreLayout
 import com.example.jetpack.data.enums.HomeShortcut
+import com.example.jetpack.lifecycleobserver.NotificationLifecycleObserver
 import com.example.jetpack.notification.LockscreenManager
 import com.example.jetpack.notification.NotificationManager
 import com.example.jetpack.repository.WeatherRepositoryImplement
@@ -31,12 +35,14 @@ import com.example.jetpack.ui.component.CoreBottomBar
 import com.example.jetpack.ui.component.CoreDialog
 import com.example.jetpack.ui.fragment.home.component.HomeDialog
 import com.example.jetpack.ui.fragment.home.component.HomeShortcutItem
-import com.example.jetpack.lifecycleobserver.NotificationLifecycleObserver
 import com.example.jetpack.ui.theme.Background
+import com.example.jetpack.ui.theme.ShimmerListItem
 import com.example.jetpack.ui.view.DigitalClock2
 import com.example.jetpack.util.NavigationUtil.safeNavigate
 import com.example.jetpack.util.PermissionUtil
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.Timer
+import java.util.TimerTask
 import javax.inject.Inject
 
 
@@ -119,11 +125,25 @@ class HomeFragment : CoreFragment() {
 }
 
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun HomeLayout(
     onOpenConfirmDialog: () -> Unit = {},
     onOpenShortcut: (HomeShortcut) -> Unit = {}
 ) {
+    var loading by remember { mutableStateOf(true) }
+
+    LaunchedEffect(key1 = loading) {
+        Timer().schedule(
+            object : TimerTask() {
+                override fun run() {
+                    loading = false
+                }
+            },
+            2000
+        )
+    }
+
     CoreLayout(
         bottomBar = { CoreBottomBar() },
         backgroundColor = Background,
@@ -152,7 +172,8 @@ fun HomeLayout(
                  DigitalClock2 ()
              }*/
 
-            items(
+
+            /*items(
                 items = HomeShortcut.entries,
                 key = { item: HomeShortcut -> item.name },
                 itemContent = { it ->
@@ -160,6 +181,20 @@ fun HomeLayout(
                         shortcut = it,
                         onClick = onOpenShortcut
                     )
+                })*/
+
+            items(
+                items = HomeShortcut.entries,
+                key = { item: HomeShortcut -> item.name },
+                itemContent = { it ->
+                    ShimmerListItem(
+                        loading = loading,
+                        contentAfterLoading = {
+                            HomeShortcutItem(
+                                shortcut = it,
+                                onClick = onOpenShortcut
+                            )
+                        })
                 })
         }
     }
---
+++ app/src/main/java/com/example/jetpack/ui/theme/ShimmerEffect.kt
@@ -0,0 +1,94 @@
+package com.example.jetpack.ui.theme
+
+import android.annotation.SuppressLint
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ShimmerListItem(
+    loading: Boolean,
+    contentAfterLoading: @Composable () -> Unit = {},
+    modifier: Modifier = Modifier
+) {
+    if (loading) {
+        Row(modifier = modifier) {
+            Box(
+                modifier = Modifier
+                    .size(100.dp)
+                    .shimmerEffect()
+            ) {
+            }
+            Spacer(modifier = Modifier.width(16.dp))
+
+            Column(modifier = Modifier.weight(1F)) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(20.dp)
+                        .shimmerEffect()
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(20.dp)
+                        .shimmerEffect()
+                )
+            }
+        }
+    } else {
+        contentAfterLoading()
+    }
+}
+
+@SuppressLint("ModifierFactoryUnreferencedReceiver")
+fun Modifier.shimmerEffect(): Modifier = composed {
+    var size by remember { mutableStateOf(IntSize.Zero) }
+
+    val transition = rememberInfiniteTransition(label = "transition")
+    val startOffsetX by transition.animateFloat(
+        initialValue = -2 * size.width.toFloat(),
+        targetValue = 2 * size.width.toFloat(),
+        animationSpec = animationInfiniteFloat,
+        label = "startOffsetX"
+    )
+
+    background(
+        brush = Brush.linearGradient(
+            colors = listOf(
+                Color(0xFFB8B5B5),
+                Color(0xFF8F8B8B),
+                Color(0xFFB8B5B5),
+            ),
+            start = Offset(x = startOffsetX, y = 0F),
+            end = Offset(x = startOffsetX + size.width.toFloat(), y = size.height.toFloat())
+        )
+    )
+        .onGloballyPositioned {
+            size = it.size
+        }
+}
