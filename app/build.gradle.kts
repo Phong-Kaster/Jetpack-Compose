3d0495c1c4df0010b8fe048fea8e173d155ad08e
phongapero <phongnt@apero.vn>
2024-05-09 10:50:24 +0700

    refactor: remove abundant dependencies in Gradle file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -92,7 +92,6 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("com.google.firebase:firebase-crashlytics-buildtools:3.0.0")
     implementation("androidx.profileinstaller:profileinstaller:1.3.1")
 
@@ -108,8 +107,8 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     // Dependency injection with Hilt - https://developer.android.com/training/dependency-injection/hilt-android#setup
-    implementation("com.google.dagger:hilt-android:2.51.1")
     // Hilt Android Processor - https://mvnrepository.com/artifact/com.google.dagger/hilt-android-compiler
+    implementation("com.google.dagger:hilt-android:2.51.1")
     kapt("com.google.dagger:hilt-android-compiler:2.51.1")
     implementation("androidx.hilt:hilt-navigation-fragment:1.2.0")
 
@@ -130,8 +129,8 @@ dependencies {
 
 
     // ConstraintLayout in Compose - https://developer.android.com/jetpack/compose/layouts/constraintlayout#get-started
-    // Motion Layout - https://developer.android.com/develop/ui/views/animations/motionlayout
-    // Motion Layout - https://medium.com/mindful-engineering/after-going-through-this-blog-youll-achieve-this-kind-of-polished-animation-using-motionlayout-6b76ec41c6ab
+    // Motion Layout 1 - https://developer.android.com/develop/ui/views/animations/motionlayout
+    // Motion Layout 2 - https://medium.com/mindful-engineering/after-going-through-this-blog-youll-achieve-this-kind-of-polished-animation-using-motionlayout-6b76ec41c6ab
     implementation("androidx.constraintlayout:constraintlayout-compose-android:1.1.0-alpha13")
 
 
--- app/src/main/java/com/example/jetpack/ui/component/CoreExpandableFloatingButton.kt
+++ app/src/main/java/com/example/jetpack/ui/component/CoreExpandableFloatingButton.kt
@@ -12,7 +12,6 @@ import androidx.compose.animation.fadeOut
 import androidx.compose.animation.shrinkVertically
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -22,10 +21,8 @@ import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.widthIn
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -34,7 +31,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
