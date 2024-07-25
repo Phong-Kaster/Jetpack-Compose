33012fb00d1f35213f9b8905ce8454808ca88932
Phong Kaster <phongnt@apero.vn>
2024-07-25 16:55:42 +0700

    docs: the A commit

--- app/src/main/java/com/example/jetpack/JetpackApplication.kt
+++ app/src/main/java/com/example/jetpack/JetpackApplication.kt
@@ -1,8 +1,14 @@
 package com.example.jetpack
 
 import android.app.Application
+import android.util.Log
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class JetpackApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        Log.d("TAG", "onCreate - the A commit")
+    }
 }
