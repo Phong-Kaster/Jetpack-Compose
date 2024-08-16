e7d40333480a561ddeb451b12811bd0feac2ae9d
Phong Kaster <phongnt@apero.vn>
2024-08-16 11:33:57 +0700

    fix: optimize Core Top Bar 3

--- app/src/main/java/com/example/jetpack/ui/component/CoreTopBar3.kt
+++ app/src/main/java/com/example/jetpack/ui/component/CoreTopBar3.kt
@@ -1,6 +1,7 @@
 package com.example.jetpack.ui.component
 
 import androidx.annotation.DrawableRes
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.basicMarquee
 import androidx.compose.foundation.layout.Arrangement
@@ -15,6 +16,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -23,6 +25,7 @@ import androidx.compose.ui.unit.dp
 import com.example.jetpack.R
 import com.example.jetpack.ui.theme.PrimaryColor
 import com.example.jetpack.ui.theme.customizedTextStyle
+import com.example.jetpack.util.ViewUtil
 
 @Composable
 fun CoreTopBar3(
@@ -51,10 +54,14 @@ fun CoreTopBar3(
                 .size(24.dp)
         ) {
             Icon(
-                painter = painterResource(id = iconLeft ?: R.drawable.ic_back),
+                painter =
+                if (iconLeft == null)
+                    painterResource(id = R.drawable.ic_back)
+                else
+                    painterResource(id = iconLeft),
                 contentDescription = null,
                 tint = iconColor,
-                modifier = Modifier.size(15.dp)
+                modifier = Modifier.size(18.dp)
             )
         }
 
@@ -82,26 +89,64 @@ fun CoreTopBar3(
 
 
 
-
-        if (iconRight != null) {
-            IconButton(
-                onClick = { onRightClick() },
-                modifier = Modifier
-                    .size(24.dp)
-            ) {
-                Icon(
-                    painter = painterResource(id = iconRight),
-                    contentDescription = null,
-                    tint = iconColor,
-                    modifier = Modifier.size(15.dp)
+        IconButton(
+            enabled = iconRight != null,
+            onClick = { onRightClick() },
+            modifier = Modifier
+                .size(24.dp)
+                .alpha(
+                    alpha = if (iconRight == null) 0f else 1f
                 )
-            }
+        ) {
+            Icon(
+                painter =
+                if (iconRight == null)
+                    painterResource(id = R.drawable.ic_forward)
+                else
+                    painterResource(id = iconRight),
+                contentDescription = null,
+                tint = iconColor,
+                modifier = Modifier.size(18.dp)
+            )
         }
+
     }
 }
 
 @Preview
 @Composable
-private fun PreviewCoreTopBar3() {
-    CoreTopBar3()
+private fun PreviewCoreTopBar3ArrangementLeft() {
+    ViewUtil.PreviewContent {
+        CoreTopBar3(
+            iconLeft = R.drawable.ic_back,
+            title = stringResource(id = R.string.app_name)
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun PreviewCoreTopBar3ArrangementCenter() {
+    ViewUtil.PreviewContent {
+        CoreTopBar3(
+            iconLeft = R.drawable.ic_back,
+            title = stringResource(id = R.string.app_name),
+            titleArrangement = Arrangement.Center,
+            iconRight = R.drawable.ic_back,
+        )
+    }
+}
+
+
+@Preview
+@Composable
+private fun PreviewCoreTopBar3ArrangementRight() {
+    ViewUtil.PreviewContent {
+        CoreTopBar3(
+            iconLeft = R.drawable.ic_back,
+            title = stringResource(id = R.string.app_name),
+            titleArrangement = Arrangement.End,
+            iconRight = R.drawable.ic_forward,
+        )
+    }
 }
