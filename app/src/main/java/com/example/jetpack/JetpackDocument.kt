eb95193023ca12c2593b2b274387b5af6f045605
Phong Kaster <phongnt@apero.vn>
2024-08-21 18:19:39 +0700

    feature: update new link for make new theme

--- app/src/main/java/com/example/jetpack/JetpackDocument.kt
+++ app/src/main/java/com/example/jetpack/JetpackDocument.kt
@@ -1,9 +1,12 @@
 package com.example.jetpack
 
+import javax.inject.Singleton
+
 
 /**
  * # [1 - 16 Kotlin Tips and Tricks for Efficient Programming](https://archive.is/SHiOv#selection-1151.0-1155.11)
  * # [2 - Theming 🎨 like a 💙Pro in Android Compose](https://freedium.cfd/https://medium.com/@niranjanky14/theming-in-android-compose-made-easy-09eb95fecf0e)
+ * # [3 - How to Create an Animated Theme Switcher in Jetpack Compose](https://medium.com/@kappdev/how-to-create-an-animated-theme-switcher-in-jetpack-compose-32c9457ee6c3)
  */
-class JetpackDocument {
-}
+@Singleton
+class JetpackDocument {}
