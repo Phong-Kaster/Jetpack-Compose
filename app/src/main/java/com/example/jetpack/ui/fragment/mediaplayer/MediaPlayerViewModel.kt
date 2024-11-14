ed05e3d5678c37b61052b1edfe07d9c861fa8cce
Phong Kaster <phongnt@apero.vn>
2024-11-14 11:57:51 +0700

    refactor: convert mutable song to immutable songs in Media Player View Model

--- app/src/main/java/com/example/jetpack/ui/fragment/mediaplayer/MediaPlayerViewModel.kt
+++ app/src/main/java/com/example/jetpack/ui/fragment/mediaplayer/MediaPlayerViewModel.kt
@@ -2,6 +2,7 @@ package com.example.jetpack.ui.fragment.mediaplayer
 
 import android.annotation.SuppressLint
 import android.content.ContentUris
+import android.content.Context
 import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Build
@@ -13,12 +14,69 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.jetpack.JetpackApplication
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.persistentListOf
+import kotlinx.collections.immutable.toImmutableList
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+/**
+ * # Collections of Media Files
+ * - Images
+ *      Stored in: DCIM/ and Pictures/ directories
+ *
+ *      Added to: MediaStore.Images table
+ *
+ * - Videos
+ *      Stored in: DCIM/, Movies/, and Pictures/ directories
+ *
+ *      Added to: MediaStore.Video table
+ *
+ * - Audio Files
+ *      Stored in:
+ *
+ *              Alarms
+ *
+ *              Audiobooks
+ *
+ *              Music
+ *
+ *              Notifications
+ *
+ *              Podcasts
+ *
+ *              Ringtones
+ *
+ *      Added to: MediaStore.Audio table
+ *
+ * - Downloaded Files
+ *      Stored in: Download/ directory
+ *
+ *      Added to: MediaStore.Downloads table (for devices running Android 10 and higher)
+ *
+ * # Special Cases
+ * Recordings directory: Audio files (e.g., voice recordings) stored in Recordings/ directory. Not available on Android 11 and lower.
+ *
+ * # MediaStore.Files Collection
+ * This collection's contents depend on whether your app uses scoped storage (Android 10 and higher).
+ *
+ * - Scoped Storage Enabled
+ *
+ *      Shows only media files that your app has created.
+ *
+ * - Scoped Storage Disabled or Unavailable
+ *
+ *      Shows all types of media files.
+ *
+ * # Access to Media Files from Other Apps
+ *
+ *      If you need to view media files created by other apps, use READ_EXTERNAL_STORAGE permission.
+ *
+ *      It's recommended to use MediaStore APIs for opening files not created by your app.
+ */
 @HiltViewModel
 class MediaPlayerViewModel
 @Inject
@@ -28,11 +86,21 @@ constructor(
 
     private val TAG = this.javaClass.simpleName
 
+    private var index = mutableIntStateOf(0)
+    private val listOfSong = mutableListOf<Song>()
+
+    /**
+     * song that users choose now
+     */
     private var _song = MutableStateFlow<Song?>(Song())
     val song = _song.asStateFlow()
 
-    private var index = mutableIntStateOf(0)
-    private val listOfSong = mutableListOf<Song>()
+    /**
+     * All songs in device
+     * */
+    private var _songs = MutableStateFlow<ImmutableList<Song>>(persistentListOf())
+    val songs = _songs.asStateFlow()
+
 
     init {
         collectSongInStorage()
@@ -43,6 +111,7 @@ constructor(
      */
     fun collectSongInStorage() {
         viewModelScope.launch(Dispatchers.IO) {
+            listOfSong.clear()
             val collection =
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
                     MediaStore.Audio.Media.getContentUri(
@@ -89,14 +158,16 @@ constructor(
                     val duration = cursor.getInt(durationColumn)
                     val size = cursor.getInt(sizeColumn)
 
-                    val uri: Uri = ContentUris.withAppendedId(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, id)
+                    val uri: Uri =
+                        ContentUris.withAppendedId(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, id)
 
                     // Load thumbnail of a specific media item.
-                    val thumbnail = loadThumbnail(uri = uri)
+                    val thumbnail: Bitmap? = loadThumbnail(context = applicationContext, uri = uri)
 
 
                     // Stores column values and the contentUri in a local object
                     // that represents the media file.
+
                     listOfSong += Song(
                         name = name,
                         duration = duration,
@@ -107,29 +178,48 @@ constructor(
                 }
             }
 
-            if (listOfSong.isEmpty()) return@launch
-
-            _song.value = listOfSong[index.intValue]
-
+            /*Log.d(TAG, "collectSongInStorage - listOfSong = ${listOfSong.size}")
             Log.d(TAG, "collectSongInStorage - listOfSong = $listOfSong")
-            Log.d(TAG, "collectSongInStorage - listOfSong = ${listOfSong.size}")
-            Log.d(TAG, "collectSongInStorage - song = ${_song.value}")
+            Log.d(TAG, "collectSongInStorage - song = ${_song.value}")*/
+
+            if (listOfSong.isEmpty()) return@launch
+            _songs.value = listOfSong.toImmutableList()
+            _song.value = listOfSong[index.intValue]
         }
     }
 
-    private fun loadThumbnail(uri: Uri): Bitmap {
-        val thumbnail: Bitmap =
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                applicationContext.contentResolver.loadThumbnail(uri, Size(640, 480), null)
-            } else {
+    /**********************************
+     * Load album art of a song
+     */
+    private fun loadThumbnail(context: Context, uri: Uri): Bitmap? {
+        // Check if the file exists val
+        val cursor = context.contentResolver.query(uri, null, null, null, null)
+        if (cursor == null || !cursor.moveToFirst()) {
+            cursor?.close()
+            throw java.io.FileNotFoundException("No album art found at the given URI: $uri")
+        }
+        cursor.close()
+        Log.d(TAG, "loadThumbnail - cursor close")
+
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            try {
+                context.contentResolver.loadThumbnail(uri, Size(640, 480), null)
+            } catch (ex: Exception) {
+                ex.printStackTrace()
+                null
+            }
+        } else {
+            try {
                 MediaStore.Images.Thumbnails.getThumbnail(
-                    applicationContext.contentResolver,
+                    context.contentResolver,
                     ContentUris.parseId(uri),
                     MediaStore.Images.Thumbnails.MINI_KIND,
                     null
                 )
+            } catch (ex: Exception) {
+                ex.printStackTrace()
+                null
             }
-
-        return thumbnail
+        }
     }
 }
