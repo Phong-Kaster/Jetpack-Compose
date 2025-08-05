29701a71d4501d25433bfae872aaa2f27388b717
Phong Kaster <phongnt@apero.vn>
2025-08-05 12:00:05 +0700

    build: use both traditional gradle & version catalog gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.12.0" apply false
+    id("com.android.application") version "8.12.0-alpha06" apply false
     id("org.jetbrains.kotlin.android") version "2.2.0" apply false
 
 
@@ -8,7 +8,7 @@ plugins {
     id("com.google.dagger.hilt.android") version "2.57" apply false
 
 
-    id("com.android.test") version "8.12.0" apply false
+    id("com.android.test") version "8.12.0-alpha06" apply false
     id("androidx.baselineprofile") version "1.4.0" apply false
 
 
@@ -21,6 +21,6 @@ buildscript {
         google()
     }
     dependencies {
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.9.3")
+        classpath(libs.navigation.safe.args.gradle.plugin)
     }
 }
